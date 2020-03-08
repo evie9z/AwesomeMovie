0d6ebc2cbe4f3b69cd6ca52c51c0cfe9f1fe0e7e
evie9z <zyw999999999@gmail.com>
2020-03-08 01:47:31 -0800

    Implement react-navigation

--- App.js
+++ App.js
@@ -1,86 +1,17 @@
-import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./src/screens/Home";
 
-import Swiper from "react-native-swiper";
+const Drawer = createDrawerNavigator();
 
-const styles = StyleSheet.create({
-  wrapper: {},
-  slide1: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#9DD6EB"
-  },
-  slide2: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#97CAE5"
-  },
-  slide3: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#92BBD9"
-  },
-  text: {
-    color: "#fff",
-    fontSize: 30,
-    fontWeight: "bold"
-  }
-});
-
-export default class SwiperComponent extends Component {
-  render() {
-    return (
-      <Swiper
-        style={styles.wrapper}
-        showsButtons={false}
-        dotColor="#FFF"
-        activeDotColor="#080808"
-      >
-        <Swiper
-          horizontal={false}
-          loop={false}
-          showsPagination={false}
-          index={0}
-        >
-          <View style={styles.slide1}>
-            <Text style={styles.text}>Movie 1</Text>
-          </View>
-          <View style={styles.slide1}>
-            <Text style={styles.text}>Details</Text>
-          </View>
-        </Swiper>
-        <Swiper
-          horizontal={false}
-          loop={false}
-          showsPagination={false}
-          index={0}
-        >
-          <View style={styles.slide2}>
-            <Text style={styles.text}>Movie 2</Text>
-          </View>
-          <View style={styles.slide2}>
-            <Text style={styles.text}>Details</Text>
-          </View>
-        </Swiper>
-        <Swiper
-          horizontal={false}
-          loop={false}
-          showsPagination={false}
-          index={0}
-        >
-          <View style={styles.slide3}>
-            <Text style={styles.text}>Movie 3</Text>
-          </View>
-          <View style={styles.slide3}>
-            <Text style={styles.text}>Details</Text>
-          </View>
-        </Swiper>
-      </Swiper>
-    );
-  }
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+        {/* <Drawer.Screen name="Notifications" component={NotificationsScreen} /> */}
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
-
-AppRegistry.registerComponent("myproject", () => SwiperComponent);
