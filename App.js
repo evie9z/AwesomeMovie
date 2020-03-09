a4c2a1095f97a6025f2dcf6653662a0a8611ae69
evie9z <zyw999999999@gmail.com>
2020-03-09 03:22:22 -0700

    Remove unnecessary comments

--- App.js
+++ App.js
@@ -21,24 +21,3 @@ const drawer = createDrawerNavigator(
 const App = createAppContainer(drawer);
 
 export default App;
-
-// import * as React from "react";
-// import { createDrawerNavigator } from "@react-navigation/drawer";
-// import { NavigationContainer } from "@react-navigation/native";
-// import Home from "./src/screens/Home";
-// import Tickets from "./src/screens/Tickets";
-// import Profile from "./src/screens/Profile";
-
-// const Drawer = createDrawerNavigator();
-
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Drawer.Navigator initialRouteName="Home">
-//         <Drawer.Screen name="Home" component={Home} />
-//         <Drawer.Screen name="My Tickets" component={Tickets} />
-//         <Drawer.Screen name="My Profile" component={Profile} />
-//       </Drawer.Navigator>
-//     </NavigationContainer>
-//   );
-// }
