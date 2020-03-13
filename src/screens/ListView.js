6e6929970cf7f1436f17c074123658327f658ad5
evie9z <zyw999999999@gmail.com>
2020-03-13 02:26:43 -0700

    Add tabs at the top

--- src/screens/ListView.js
+++ src/screens/ListView.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 import SwiperHeader from "../components/SwiperHeader";
+import ListViewTab from "../components/ListViewTabs";
+
+const { width } = Dimensions.get("window");
 
 export default class Index extends Component {
+  state = {
+    isInTheater: true
+  };
+  handleTabIndexChange = () => {
+    console.log("Gets to handleTabIndexChange");
+    this.setState({
+      isInTheater: !this.state.isInTheater
+    });
+  };
   render() {
+    console.log("isInTheater after: " + this.state.isInTheater);
     return (
       <View style={styles.container}>
         <View style={styles.headerContainer}>
           <SwiperHeader navigation={this.props.navigation} />
+        </View>
+        <View style={styles.tabContainer}>
+          <ListViewTab
+            handleTabIndexChange={this.handleTabIndexChange}
+            isInTheater={this.state.isInTheater}
+          />
         </View>
         <View style={styles.textContainer}>
           <Text style={styles.title}>List View!</Text>
@@ -23,7 +42,17 @@ const styles = StyleSheet.create({
     backgroundColor: "#2C2C2C"
   },
   headerContainer: {
-    zIndex: 1
+    zIndex: 1,
+    backgroundColor: "#2C2C2C",
+    height: 60,
+    width: width
+  },
+  tabContainer: {
+    backgroundColor: "#2C2C2C",
+    height: 50,
+    width: width,
+    justifyContent: "center",
+    alignItems: "center"
   },
   textContainer: {
     flex: 1,
